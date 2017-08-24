b36a1d532c6c49cc8856817317042f53f4474932
Allison Thompson <allison.thompson@pnnl.gov>
2017-08-24 10:19:07 -0700

    Make ellipses optional in NMDS plot

--- R/pmartRseq_NMDS.R
+++ R/pmartRseq_NMDS.R
@@ -4,6 +4,7 @@
 #'
 #' @param res an object created by vegan::metaMDS
 #' @param grp vector of grouping variables
+#' @param ellipses logical indicating whether or not to show ellipses on the plot
 #'
 #' @details After beta diversity is calculated, this function will create a plot of the results, along with ellipses, to show the different groupings.
 #'
@@ -24,7 +25,7 @@
 #' @author Allison Thompson
 #'
 #' @export
-pmartRseq_NMDS <- function(res,grp){
+pmartRseq_NMDS <- function(res,grp,ellipses){
 
   library(ggplot2)
 
@@ -53,35 +54,56 @@ pmartRseq_NMDS <- function(res,grp){
   NMDS <- data.frame(NMDS1, NMDS2, Treatment)
   NMDS.mean = aggregate(NMDS[,1:2], list(group=Treatment), mean)
 
-  # Function for drawing ellipses
-  veganCovEllipse <- function(cov, center = c(0, 0), scale = 1, npoints = 100){
-    theta <- (0:npoints) * 2 * pi/npoints
-    Circle <- cbind(cos(theta), sin(theta))
-    t(center + scale * t(Circle %*% chol(cov)))
-  }
+  if(ellipses){
+    # Function for drawing ellipses
+    veganCovEllipse <- function(cov, center = c(0, 0), scale = 1, npoints = 100){
+      theta <- (0:npoints) * 2 * pi/npoints
+      Circle <- cbind(cos(theta), sin(theta))
+      t(center + scale * t(Circle %*% chol(cov)))
+    }
 
-  # Create a new dataframe with data and ellipses
-  df_ell <- data.frame()
-  for(g in levels(NMDS$Treatment)){
-    df_ell <- rbind(df_ell,
-                    cbind(as.data.frame(with(NMDS[NMDS$Treatment==g,],
-                    veganCovEllipse(cov.wt(cbind(NMDS1,NMDS2),wt=rep(1/length(NMDS1),length(NMDS1)))$cov,center=c(mean(NMDS1),mean(NMDS2))))),
-                    group=g))
-  }
+    # Create a new dataframe with data and ellipses
+    df_ell <- data.frame()
+    for(g in levels(NMDS$Treatment)){
+      df_ell <- rbind(df_ell,
+                      cbind(as.data.frame(with(NMDS[NMDS$Treatment==g,],
+                      veganCovEllipse(cov.wt(cbind(NMDS1,NMDS2),wt=rep(1/length(NMDS1),length(NMDS1)))$cov,center=c(mean(NMDS1),mean(NMDS2))))),
+                      group=g))
+    }
 
-  # Plot
-  X1 <- ggplot(data = NMDS, aes(NMDS1, NMDS2)) +
-    geom_point(aes(color = Treatment), size=2.5, alpha=0.75) +
-    geom_path(data=df_ell, aes(x=NMDS1, y=NMDS2, colour=group), size=1.5, linetype=5)+
-    theme_bw()+
-    theme(aspect.ratio=1,
-          axis.text.x=element_text(size=20),
-          axis.text.y=element_text(size=20),
-          axis.title.x=element_text(size=20),
-          axis.title.y=element_text(size=20),
-          legend.title=element_text(size=15),
-          legend.text=element_text(size=15),
-          panel.grid=element_blank())
+    # Plot
+    X1 <- ggplot(data = NMDS, aes(NMDS1, NMDS2)) +
+      geom_point(aes(color = Treatment), size=2.5, alpha=1) +
+      geom_path(data=df_ell, aes(x=NMDS1, y=NMDS2, colour=group), size=1.5, linetype=5, alpha=0.7)+
+      theme_bw()+
+      theme(aspect.ratio=1,
+            axis.text.x=element_text(size=20),
+            axis.text.y=element_text(size=20),
+            axis.title.x=element_text(size=20),
+            axis.title.y=element_text(size=20),
+            legend.title=element_text(size=15),
+            legend.text=element_text(size=15),
+            panel.grid=element_blank())
+  }else{
+    df_ell <- data.frame()
+    for(g in levels(NMDS$Treatment)){
+      df_ell <- rbind(df_ell,
+                      cbind(as.data.frame(NMDS[NMDS$Treatment==g,],group=g)))
+    }
+
+    # Plot
+    X1 <- ggplot(data = NMDS, aes(NMDS1, NMDS2)) +
+      geom_point(aes(color = Treatment), size=2.5, alpha=0.75) +
+      theme_bw()+
+      theme(aspect.ratio=1,
+            axis.text.x=element_text(size=20),
+            axis.text.y=element_text(size=20),
+            axis.title.x=element_text(size=20),
+            axis.title.y=element_text(size=20),
+            legend.title=element_text(size=15),
+            legend.text=element_text(size=15),
+            panel.grid=element_blank())
+  }
   X1
 }
 

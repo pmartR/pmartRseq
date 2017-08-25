a272d60e55b1d6971dd413b62c89bf2653c2e86c
Allison Thompson <allison.thompson@pnnl.gov>
2017-08-25 16:44:02 -0700

    Add indicator species analysis and plot functionality

---
+++ R/indsp_calc.R
@@ -0,0 +1,122 @@
+#' Computes Indicator Species Analysis for Count Data
+#'
+#' This function is a wrapper for an indicator species analysis using the package \code{indicspecies}, studying the association between species patterns and combinations of group sites.
+#'
+#' @param omicsData an object of the class 'seqData' created by \code{\link{as.seqData}}.
+#' @param within If desired, can run indicator species analysis between one effect and within another effect.
+#' @param pval_thresh P-value threshold for creating flags for significance. The default is 0.05.
+#'
+#' @details This function uses the \code{multipatt} function in the package \code{indicspecies} to create combinations of input clusters and compare each combination with the species in \code{e_data}. Please refer to the \code{multipatt} function in the package \code{indicspecies} for more information.
+#'
+#' @return An object of class indspRes (also a data.frame) containing the indicator species analysis of the data.
+#'
+#' @references De Cáceres, M. and Legendre, P. 2009. Associations between species and groups of sites: indices and statistical inference. Ecology 90(12): 3566-3574.
+#'
+#' @examples
+#' \dontrun{
+#' library(mintJansson)
+#' data(rRNA_data)
+#' rRNA_data <- group_designation(omicsData = rRNA_data, main_effects = c("site", "treatment"), time_course=NULL)
+#'
+#' rRNA_indsp <- indsp_calc(omicsData = rRNA_data, within=NULL, pval_thresh=0.05)
+#' head(rRNA_indsp)
+#' summary(rRNA_indsp)
+#' plot(rRNA_indsp)
+#' }
+#'
+#' @author Allison Thompson
+#'
+#' @export
+indsp_calc <- function(omicsData, within=NULL, pval_thresh=0.05){
+  library(indicspecies)
+
+    ## initial checks ##
+
+  # check that omicsData is of appropriate class #
+  if(!class(omicsData) %in% c("seqData")) stop("omicsData must be of class 'seqData'")
+
+  if(attr(omicsData, "data_info")$data_scale!='count'){
+    warning("This function is meant for count data like 'rRNA', 'gDNA' or 'cDNA' data.")
+  }
+
+  if(!attr(omicsData, "data_info")$data_norm){
+    warning("We suggest normalizing before running this function.")
+  }
+
+  if(is.null(attr(omicsData, "group_DF"))){
+    stop("Need to run group_designation function first.")
+  }
+
+  ## end of initial checks ##
+
+  # format e_data
+  #indspec <- mintR:::helper_edata_replace(omicsData, x=NA , y=0)$e_data
+  indspec <- omicsData$e_data
+  indspec[which(is.na(indspec))] <- 0
+
+  rownames(indspec) <- omicsData$e_data[,attr(omicsData, "cnames")$edata_cname]
+  indspec <- indspec[,-which(colnames(indspec) == attr(omicsData,"cnames")$edata_cname)]
+
+  indspec <- t(indspec)
+  indspec <- as.data.frame(indspec)
+
+  # format groups
+  group <- attr(omicsData, "group_DF")$Group[match(rownames(indspec), attr(omicsData,"group_DF")[,attr(omicsData,"cnames")$fdata_cname])]
+
+  if(is.null(within)){
+    # indicator species analysis from indicspecies
+    IS <- multipatt(indspec, group, func="IndVal.g", control=how(nperm=999))
+
+    # format results
+    results <- IS$sign
+    results$Flag <- ifelse(results$p.value <= pval_thresh, 1, 0)
+
+  }else{
+    attr(omicsData,"group_DF")[,within] <- as.factor(attr(omicsData,"group_DF")[,within])
+    fdata_cname <- attr(omicsData, "cnames")$fdata_cname
+    IS <- lapply(levels(attr(omicsData, "group_DF")[,within]), function(x){
+      # subset grouping information to only those samples within a specified group
+      meta.data <- subset(attr(omicsData, "group_DF"), get(within) == x )
+
+      # subset data to only those samples within a specified group
+      indspec2 <- indspec[which(rownames(indspec) %in% meta.data[,fdata_cname]), ]
+
+      # re-order meta.data
+      meta.data <- meta.data[match(rownames(indspec2),meta.data[,fdata_cname]),]
+
+      # indicator species analysis
+      IS <- multipatt(indspec2, as.character(meta.data$Group), func="IndVal.g", control=how(nperm=999))
+      results <- IS$sign
+      results$Flag <- ifelse(results$p.value <= pval_thresh, 1, 0)
+
+      # format results
+      idx1 <- grep("index", colnames(results))
+      idx2 <- grep("stat", colnames(results))
+      idx3 <- grep("p.value", colnames(results))
+      idx4 <- grep("Flag", colnames(results))
+      idx <- c(idx1, idx2, idx3, idx4)
+      colnames(results)[idx] <- unlist(lapply(idx, function(y) paste(colnames(results)[y], x, sep="_")))
+
+      # order results so cbind doesn't mess anything up
+      results <- results[order(rownames(results)),]
+      return(results)
+    })
+    results <- do.call(cbind, IS)
+  }
+
+  # results <- cbind(rownames(results), results)
+  # colnames(results)[1] <- attr(omicsData, "cnames")$edata_cname
+  # rownames(results) <- NULL
+  #
+  res <- results
+
+  # make an indicator species object
+  attr(res, "group_DF") <- attr(omicsData, "group_DF")
+  attr(res, "cnames") <- attr(omicsData, "cnames")
+  attr(res, "Threshold") <- pval_thresh
+  attr(res, "within") <- within
+  class(res) <- c("indspRes", class(results))
+
+  return(res)
+
+}
---
+++ R/plot_indsp.R
@@ -0,0 +1,126 @@
+#' Plot Indicator species Analysis Results
+#'
+#' This function plots the results from an indicator species analysis.
+#'
+#' @param indsp an indicator species analysis results object, created by \code{\link{indsp_calc}}
+#' @param omicsData an object of the class 'seqData' created by \code{\link{as.seqData}}.
+#' @param x_axis which grouping value to put on the x-axis. Default is "Group".
+#' @param group Which taxonomic level to group the results by. Default is "Phylum".
+#'
+#' @details This function creates a heatmap-type plot showing the results of an indicator species analysis.
+#'
+#' @return A plot of indicator species analysis results.
+#'
+#' @references De Cáceres, M. and Legendre, P. 2009. Associations between species and groups of sites: indices and statistical inference. Ecology 90(12): 3566-3574.
+#'
+#' @examples
+#' \dontrun{
+#' library(mintJansson)
+#' data(rRNA_data)
+#' rRNA_data <- group_designation(omicsData = rRNA_data, main_effects = c("site", "treatment"), time_course=NULL)
+#' rRNA_norm <- normalize_data(omicsData = rRNA_data, norm_fn = "css", normalize = TRUE)
+#'
+#' rRNA_indsp <- indsp_calc(omicsData = rRNA_norm, within=NULL, pval_thresh=0.05)
+#' head(rRNA_indsp)
+#' summary(rRNA_indsp)
+#' plot_indsp(indsp = rRNA_indsp, omicsData = rRNA_data)
+#' }
+#'
+#' @author Allison Thompson
+#'
+#' @export
+
+plot_indsp <- function(indsp, omicsData, x_axis = "Group", group = "Phylum"){
+
+  library(reshape)
+  library(dplyr)
+  library(ggplot2)
+
+  # format normalized data
+  attr(omicsData, "group_DF")$Group <- gsub("-","\\.",attr(omicsData, "group_DF")$Group)
+  groupDF <- unique(attr(omicsData, "group_DF")[,-which(colnames(attr(omicsData, "group_DF")) == attr(omicsData,"cnames")$fdata_cname)])
+  normdata <- omicsData$e_data
+  normdata <- melt(normdata)
+  normdata <- merge(normdata, attr(omicsData,"group_DF"), by.x="variable", by.y=attr(omicsData,"cnames")$fdata_cname)
+
+  # get mean normalized abundance for every group for every feature
+  vars <- c(attr(omicsData,"cnames")$edata_cname, x_axis)
+  vars <- lapply(vars, as.symbol)
+  normgp <- normdata %>%
+    group_by_(.dots=vars) %>%
+    summarise(MeanNorm=mean(value, na.rm=TRUE))
+
+  # format indicator species results
+  colnames(indsp) <- gsub("-","\\.",colnames(indsp))
+  myinds <- data.frame(OTU=rownames(indsp), indsp)
+  colnames(myinds)[1] <- attr(omicsData, "cnames")$edata_cname
+
+  # separate out the necessary information
+  if(!is.null(attr(indsp,"within"))){
+    alldata <- lapply(unique(attr(omicsData,"group_DF")[,attr(indsp,"within")]), function(x){
+                    temp <- myinds[,c(1,grep(x, colnames(myinds)))]
+                    ids <- grep("s\\.",colnames(temp))
+                    temp <- merge(melt(temp[,c(1,ids)]), temp[,-ids], by=attr(omicsData,"cnames")$edata_cname)
+                    colnames(temp)[grep("index",colnames(temp))] <- "index"
+                    colnames(temp)[grep("stat",colnames(temp))] <- "stat"
+                    colnames(temp)[grep("p.value",colnames(temp))] <- "p.value"
+                    colnames(temp)[grep("Flag",colnames(temp))] <- "Flag"
+                    return(temp)
+                  })
+    alldata <- do.call(rbind, alldata)
+  }else{
+    ids <- grep("s\\.",colnames(myinds))
+    alldata <- merge(melt(myinds[,c(1,inds)]), myinds[,-inds], by=attr(omicsData,"cnames")$edata_cname)
+  }
+
+  # comine indicator species results with e_meta information
+  alldata <- merge(alldata, omicsData$e_meta, by=attr(omicsData, "cnames")$edata_cname)
+  alldata$variable <- gsub("s\\.","",alldata$variable)
+  alldata <- merge(alldata, groupDF, by.x = "variable", by.y = "Group")
+  colnames(alldata)[which(colnames(alldata)=="variable")] = "Group"
+
+  # separate out only the significant features
+  alldata$indsp <- sapply(c(1:nrow(alldata)), function(x){
+    ifelse(alldata$value[x]  == 1 & alldata$p.value[x] <= attr(indsp, "Threshold") & !is.na(alldata$p.value[x]), 1, 0)
+  })
+  alldata$indsp[is.na(alldata$indsp)] <- 0
+
+  # combine indicator species results with normalized abundances
+  alldata <- merge(alldata, normgp, by=c(attr(omicsData,"cnames")$edata_cname, "Group"))
+
+  # separate out only the significant features
+  sig <- unique(alldata[which(alldata$indsp == 1),attr(omicsData,"cnames")$edata_cname])
+  alldata <- alldata[which(alldata[,attr(omicsData,"cnames")$edata_cname] %in% sig),]
+
+  # order the important variables
+  alldata[,group] <- as.factor(alldata[,group])
+  alldata <- alldata[order(alldata[,group]),]
+  alldata[,attr(omicsData,"cnames")$edata_cname] <- factor(alldata[,attr(omicsData,"cnames")$edata_cname],
+                                                           levels=unique(alldata[order(alldata[,group]),attr(omicsData,"cnames")$edata_cname]))
+
+  # make better colors
+  TaxonBarPallette <- c("#FF00BB","#CC00FF","#F2BFFF","#7A0099","#0022FF","#8091FF","#001499","#00F2FF","#CCFCFF","#009199","#00D90E","#BFFFC4","#007308","#FFFF00","#DDFF00","#B3B300","#FF9100","#FFC880","#995700","#FF0000","#FFABAB","#990000","#BFBFBF","#636363","#000000")
+
+  map <- aes_string(x=x_axis, y=attr(omicsData,"cnames")$edata_cname, fill=group)
+  ggplot(alldata, map)+
+    geom_tile(alpha=0.25)+
+    geom_tile(aes(alpha=ifelse(indsp==1,1,0)))+
+    guides(alpha=FALSE)+
+    scale_fill_manual(values=rep(TaxonBarPallette,3))+
+    #geom_text(aes(label=round(MeanNorm, digits=1)), cex=3)+
+    theme_bw()+
+    theme(panel.grid.major=element_blank(),
+          panel.grid.minor=element_blank(),
+          axis.title=element_text(size=18, face="bold"),
+          title=element_text(size=18, face="bold"),
+          legend.title=element_text(size=18, face="bold"),
+          axis.text.x=element_text(size=9,angle=45,vjust=1,hjust=1),
+          axis.text.y=element_text(size=7),
+          legend.text=element_text(size=12))+
+    labs(title="Indicator Species", y=attr(omicsData,"cnames")$edata_cname, x=x_axis)+
+    scale_y_discrete(limits=levels(alldata[,attr(omicsData,"cnames")$edata_cname]))+
+    #coord_flip()+
+    theme(aspect.ratio=7/3)
+
+
+}

ff09a04694be77806fc853594354e8bea0c4a387
Allison Thompson <allison.thompson@pnnl.gov>
2017-08-31 14:23:16 -0700

    Add split_emeta for formatting of e_meta

---
+++ R/split_emeta.R
@@ -0,0 +1,83 @@
+#' Splits an e_meta column into separate columns
+#'
+#' This function can take one large string (like a taxonomy) and split it into a desired number of different columns. For instance, split a large string containing a taxonomy into columns for Kingdom, Phylum, etc.
+#'
+#' @param omicsData an object of one of the class "seqData"
+#' @param cname the column in e_meta to split. If NULL, will use attr(omicsData,"cnames")$emeta_cname
+#' @param split1 variable used to split string into columns. Default is ";".
+#' @param numcol number of columns to split given column into. If NULL, will calculate from data. Default is NULL.
+#' @param split2 variable used to split string again, if needed. For example, can split "k__Bacteria" into "Bacteria" and will remove the "k__" part. Default is "__".
+#' @param num which part to keep after splitting for the second time. Default is 2.
+#' @param newnames new column names after splitting is done. If NULL, will use c("Kingdom","Phylum","Class","Order","Family","Genus","Species"). Default is NULL.
+#'
+#' @return seqData object with a split e_meta
+#'
+#' @author Allison Thompson
+#'
+#' @examples
+#' \dontrun{
+#' library(mintJansson)
+#' load(rRNA_data)
+#'
+#' omicsData <- rRNA_data
+#' head(omicsData$e_meta)
+#'
+#' omicsData_split <- split_emeta(omicsData, emeta_cname="Taxonomy", split1=";", numcol=7, split2="__", num=2, newnames=NULL)
+#' head(omicsData_split$e_meta)
+#' }
+#'
+#' @export
+split_emeta <- function(omicsData, cname=NULL, split1=";", numcol=NULL, split2="__", num=2, newnames=NULL){
+  library(stringr)
+
+  ## inital checks ##
+  if(is.null(omicsData$e_meta)){
+    stop("e_meta must contain data")
+  }
+
+  emeta <- omicsData$e_meta
+
+  if(is.null(cname)){
+    cname <- attr(omicsData,"cnames")$taxa_cname
+  }
+
+  if(!is.null(split1)){
+    if(is.null(numcol)){
+      numcol <- max(unlist(lapply(lapply(emeta[,which(colnames(emeta)==cname)], function(x){
+        strsplit(as.character(x), split1)
+        }), function(y){
+          length(y[[1]])
+          }
+      )))
+    }
+  }else{
+    numcol <- ncol(emeta) - 1
+  }
+
+
+  if(!is.null(split1)){
+    split_data <- str_split_fixed(emeta[,cname], split1, numcol)
+  }else{
+    split_data <- emeta[,-which(colnames(emeta)==cname)]
+  }
+
+  # if necessary, can split again on another character, this time will remove other part
+  if(!is.null(split2)){
+    split_data <- apply(split_data, 1:2, function(x) strsplit(as.character(x), split2)[[1]][num])
+  }
+
+  split_data <- as.data.frame(split_data)
+
+  res <- omicsData
+  res$e_meta <- cbind(emeta, split_data)
+
+  # update column names
+  if(is.null(newnames)){
+    names.vec <- c("Kingdom","Phylum","Class","Order","Family","Genus","Species")
+    colnames(res$e_meta) <- c(colnames(emeta),names.vec[1:numcol])
+  }else{
+    colnames(res$e_meta) <- c(colnames(emeta),newnames)
+  }
+
+  return(res)
+}

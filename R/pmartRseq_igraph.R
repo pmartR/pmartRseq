8184f6edfc2006687012db934befccbda6b73ea1
Allison Thompson <allison.thompson@pnnl.gov>
2017-12-14 15:11:05 -0800

    Make igraph object fn

---
+++ R/pmartRseq_igraph.R
@@ -0,0 +1,85 @@
+#' Calculates a network graph
+#'
+#' This function calculates a network graph, using igraph, in order to create a network plot and/or calcuate network indices.
+#'
+#' @param netData an object of class 'netData', created by \code{link{network_calc}}
+#' @param coeff Optional, cutoff value to use for the correlation coefficient
+#' @param pval Optional, cutoff value to use for p-values
+#' @param qval Optional, cutoff value to use for q-values
+#'
+#' @details Create an igraph object, which can then be used to visualize a network plot and/or calculate network indices.
+#'
+#' @return An object of class networkGraph (also a data.frame or list) containing the network.
+#'
+#' @examples
+#' \dontrun{
+#' library(mintJansson)
+#' data(rRNA_data)
+#' mynetwork <- network_calc(omicsData = rRNA_data)
+#' myNetGraph <- pmartRseq_igraph(mynetwork)
+#' }
+#'
+#' @author Allison Thompson
+#'
+#' @export
+
+pmartRseq_igraph <- function(netData, coeff=NULL, pval=NULL, qval=NULL){
+  library(igraph)
+ # Function to make an igraph object from a network object
+
+  net <- netData
+
+  # Correlation Coefficient cutoff
+  if(!is.null(coeff)){
+    net <- subset(net, abs(cor.coeff) >= coeff)
+  }
+  # p value cutoff
+  if(!is.null(pval)){
+    net <- subset(net, p.value <= pval)
+  }
+  # q value cutoff
+  if(!is.null(qval)){
+    net <- subset(net, q.value <= qval)
+  }
+
+  # If network was created in groups, create object in groups.
+  if(!is.null(attr(netData, "group_var"))){
+
+    gN <- lapply(unique(net$Group), function(x){
+      tmp <- subset(net, Group == x)
+
+      gN <- simplify(graph.edgelist(as.matrix(tmp[,c("Row","Column")]), directed = FALSE))
+
+      #In our graph, carry through correlation coefficients
+      E(gN)$strength <- abs(tmp$cor.coeff)
+      E(gN)$dirct <- tmp$cor.coeff
+
+      return(gN)
+    })
+
+    names(gN) <- unique(net$Group)
+
+    attr(gN, "group_var") <- attr(netData, "group_var")
+
+  }else{
+    # Otherwise, create object over all data.
+    tmp <- net
+
+    gN <- simplify(graph.edgelist(as.matrix(tmp[,c("Row","Column")]), directed = FALSE))
+
+    #In our graph, carry through correlation coefficients
+    E(gN)$strength <- abs(tmp$cor.coeff)
+    E(gN)$dirct <- tmp$cor.coeff
+
+
+  }
+
+  # Attach thresholds as attribute
+  attr(gN, "thresholds") <- list(CorCoeff=coeff, PValue=pval, QValue=qval)
+
+  # Make class networkGraph
+  class(gN) <- c("networkGraph",class(gN))
+
+  return(gN)
+
+}

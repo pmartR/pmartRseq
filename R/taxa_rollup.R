a3c44da4075c144086db7a5325de3bf6b6eec9e7
thom040 <allison.thompson@pnnl.gov>
2018-01-03 16:24:32 -0800

    Make it possible to use this if you don't actually want to roll up

--- R/taxa_rollup.R
+++ R/taxa_rollup.R
@@ -55,62 +55,72 @@ taxa_rollup <- function(omicsData, level, taxa_levels=NULL){
 
   ## end initial checks ##
 
-  numlevel <- which(taxa_levels %in% tolower(level))
+  if(level == attr(omicsData, "cnames")$edata_cname){
 
-  # Format data
-  e_meta <- omicsData$e_meta
-  e_meta <- e_meta[,c(1:which(colnames(e_meta)==level))]
-  e_meta <- apply(e_meta, 1:2, function(x) as.character(x))
+    data <- omicsData
 
-  # Make a new taxonomy column
-  newTaxa_data <- e_meta[,which(tolower(colnames(e_meta)) %in% taxa_levels)]
-  newTaxa <- data.frame(ID=omicsData$e_meta[,attr(omicsData,"cnames")$edata_cname],
-                        newTaxa=unlist(lapply(c(1:nrow(newTaxa_data)), function(y) paste(newTaxa_data[y,1:numlevel], collapse=";"))))
+  }else{
 
-  e_meta <- as.data.frame(e_meta)
-  e_meta <- merge(e_meta, newTaxa, by.x=attr(omicsData,"cnames")$edata_cname, by.y="ID")
+    numlevel <- which(taxa_levels %in% tolower(level))
 
-  # Roll up data to appropriate level
-  rollup <- lapply(1:length(levels(e_meta$newTaxa)), function(x){
-    features <- e_meta[which(e_meta$newTaxa == levels(e_meta$newTaxa)[x]),attr(omicsData, "cnames")$edata_cname]
-    sub <- omicsData$e_data[which(omicsData$e_data[,attr(omicsData,"cnames")$edata_cname] %in% features),]
+    # Format data
+    e_meta <- omicsData$e_meta
+    e_meta <- e_meta[,c(1:which(colnames(e_meta)==level))]
+    e_meta <- apply(e_meta, 1:2, function(x) as.character(x))
 
-    rollup <- t(as.data.frame(colSums(sub[,-which(colnames(sub)==attr(omicsData,"cnames")$edata_cname)], na.rm=TRUE)))
-    rollup <- data.frame(Level=paste(level,x,sep=""), rollup)
-    rownames(rollup) <- NULL
+    # Make a new taxonomy column
+    newTaxa_data <- e_meta[,which(tolower(colnames(e_meta)) %in% taxa_levels)]
+    newTaxa <- data.frame(ID=omicsData$e_meta[,attr(omicsData,"cnames")$edata_cname],
+                          newTaxa=unlist(lapply(c(1:nrow(newTaxa_data)), function(y) paste(newTaxa_data[y,1:numlevel], collapse=";"))))
 
-    return(rollup)
-  })
+    e_meta <- as.data.frame(e_meta)
+    e_meta <- merge(e_meta, newTaxa, by.x=attr(omicsData,"cnames")$edata_cname, by.y="ID")
+
+    # Roll up data to appropriate level
+    rollup <- lapply(1:length(levels(e_meta$newTaxa)), function(x){
+      features <- e_meta[which(e_meta$newTaxa == levels(e_meta$newTaxa)[x]),attr(omicsData, "cnames")$edata_cname]
+      sub <- omicsData$e_data[which(omicsData$e_data[,attr(omicsData,"cnames")$edata_cname] %in% features),]
+
+      rollup <- t(as.data.frame(colSums(sub[,-which(colnames(sub)==attr(omicsData,"cnames")$edata_cname)], na.rm=TRUE)))
+      rollup <- data.frame(Level=paste(level,x,sep=""), rollup)
+      rownames(rollup) <- NULL
+
+      return(rollup)
+    })
 
 
-  # Format new edata
-  e_data <- do.call(rbind, rollup)
-  names(e_data)[1] <- paste(level,"Id",sep="")
-  e_data[e_data==0] <- NA
+    # Format new edata
+    e_data <- do.call(rbind, rollup)
+    names(e_data)[1] <- paste(level,"Id",sep="")
+    e_data[e_data==0] <- NA
 
-  # Format new emeta
-  e_meta <- e_meta[!duplicated(e_meta$newTaxa), ]
-  e_meta <- e_meta[match(levels(e_meta$newTaxa),e_meta$newTaxa),]
-  # Rename ID column
-  e_meta[,attr(omicsData, "cnames")$edata_cname] <- paste(level,c(1:nrow(e_meta)), sep="")
-  names(e_meta)[which(names(e_meta)==attr(omicsData,"cnames")$edata_cname)] <- paste(level,"Id",sep="")
-  rownames(e_meta) <- NULL
-  e_meta <- e_meta[,-which(colnames(e_meta)=="newTaxa")]
+    # Format new emeta
+    e_meta <- e_meta[!duplicated(e_meta$newTaxa), ]
+    e_meta <- e_meta[match(levels(e_meta$newTaxa),e_meta$newTaxa),]
+    # Rename ID column
+    e_meta[,attr(omicsData, "cnames")$edata_cname] <- paste(level,c(1:nrow(e_meta)), sep="")
+    names(e_meta)[which(names(e_meta)==attr(omicsData,"cnames")$edata_cname)] <- paste(level,"Id",sep="")
+    rownames(e_meta) <- NULL
+    e_meta <- e_meta[,-which(colnames(e_meta)=="newTaxa")]
 
-  # Run function to make pmartRseq object again
-  if(class(omicsData) == "seqData"){
-    data <- as.seqData(e_data=e_data, f_data=omicsData$f_data, e_meta=e_meta, edata_cname=paste(level,"Id",sep=""),
-                        taxa_cname=level, ec_cname=attr(omicsData,"cnames")$ec_cname, gene_cname=attr(omicsData,"cnames")$gene_cname,
-                        fdata_cname=attr(omicsData,"cnames")$fdata_cname, data_type=attr(omicsData,"data_info")$data_type,
-                        data_scale=attr(omicsData,"data_info")$data_scale, data_types=attr(omicsData,"data_info")$data_types,
-                        db=attr(omicsData,"database")$db, db_version=attr(omicsData,"database")$db_version,
-                        data_norm=attr(omicsData,"data_info")$data_norm, norm_method=attr(omicsData,"data_info")$norm_method,
-                        location_param=attr(omicsData,"data_info")$location_param, scale_param=attr(omicsData,"data_info")$scale_param)
-    attr(data,"group_DF") <- attr(omicsData, "group_DF")
+    # Run function to make pmartRseq object again
+    if(class(omicsData) == "seqData"){
+      data <- as.seqData(e_data=e_data, f_data=omicsData$f_data, e_meta=e_meta, edata_cname=paste(level,"Id",sep=""),
+                          taxa_cname=level, ec_cname=attr(omicsData,"cnames")$ec_cname, gene_cname=attr(omicsData,"cnames")$gene_cname,
+                          fdata_cname=attr(omicsData,"cnames")$fdata_cname, data_type=attr(omicsData,"data_info")$data_type,
+                          data_scale=attr(omicsData,"data_info")$data_scale, data_types=attr(omicsData,"data_info")$data_types,
+                          db=attr(omicsData,"database")$db, db_version=attr(omicsData,"database")$db_version,
+                          data_norm=attr(omicsData,"data_info")$data_norm, norm_method=attr(omicsData,"data_info")$norm_method,
+                          location_param=attr(omicsData,"data_info")$location_param, scale_param=attr(omicsData,"data_info")$scale_param)
+      attr(data,"group_DF") <- attr(omicsData, "group_DF")
+    }
+
+    attr(data, "rollup")$text = paste("Data was rolled up to the ", level, " level", sep="")
+    attr(data, "rollup")$level = level
+
   }
 
-  attr(data, "rollup")$text = paste("Data was rolled up to the ", level, " level", sep="")
-  attr(data, "rollup")$level = level
+  data$e_data[is.na(data$e_data)] <- 0
 
   return(data)
 

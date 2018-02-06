d55c262bd3ee063fe4637a54178bfe74d4e69a53
Reehl, Sarah <sarah.reehl@pnnl.gov>
2018-02-06 09:25:26 -0800

    adding tests

---
+++ tests/testthat/test-imports.R
@@ -0,0 +1,16 @@
+context("test-imports.R")
+
+test_that("import file checks work", {
+  expect_error(import_seqData(e_data_filepath = "doesn't matter", f_data_filepath = "doesn't matter"),
+               "Unsupported biom format. Must end in .biom, .csv, or .txt")
+  expect_error(import_seqData(f_data_filepath = "doesn't matter", e_data_filepath = "something.biom"),
+               "Unsupported sample information format. Must end in .csv or .txt")
+})
+
+test_that("import works", {
+  imported_data <- import_seqData(e_data_filepath = "test_OTU.csv", f_data_filepath = "test_Meta.csv",
+                                  e_meta_filepath = "test_Taxa.csv")
+  expect_equal(imported_data$guessed_edata_cname, "OTU.ID")
+  expect_length(imported_data, 6)
+  expect_true(all(c("e_data", "f_data", "e_meta") %in% names(imported_data)))
+})
---
+++ tests/testthat/test-structure.R
@@ -0,0 +1,15 @@
+context("test-structure.R")
+
+test_that("converting import works", {
+  imported_data <- import_seqData(e_data_filepath = "test_OTU.csv", f_data_filepath = "test_Meta.csv",
+                                  e_meta_filepath = "test_Taxa.csv")
+  structured_data <- as.seqData(e_data = imported_data$e_data,
+                               f_data = imported_data$f_data,
+                               e_meta = imported_data$e_meta,
+                               edata_cname = imported_data$guessed_edata_cname,
+                               fdata_cname = imported_data$guessed_fdata_cname,
+                               taxa_cname = imported_data$guessed_taxa_cname,
+                               data_type = "rRNA")
+  expect_output(str(structured_data), "List")
+  expect_is(structred_data, "seqData")
+})

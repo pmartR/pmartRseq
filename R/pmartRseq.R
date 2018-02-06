b85f81ff86fef1add2eb52c1f8213e2bd5c06ac1
Reehl, Sarah <sarah.reehl@pnnl.gov>
2018-02-06 09:24:52 -0800

    adding package dicumentation

---
+++ R/pmartRseq.R
@@ -0,0 +1,11 @@
+#' pmartRseq: A pipeline package that enables a reproducible workflow and
+#' analysis of 16S data.
+#'
+#' The functions in this package are also avavilable as a
+#' dashboard that allows for a complete analysis with no programming required.
+#' There are four major components to this package: input and grouping,
+#' filtering, normalization, and analysis.
+#'
+#' @docType package
+#' @name pmartRseq
+NULL
